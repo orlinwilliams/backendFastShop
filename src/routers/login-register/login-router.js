c93e80b8584b37696883f2cf4349c296721bf444
orlinwilliams <orlinwilliams0077@gmail.com>
2020-12-02 00:51:15 -0600

    fix login

--- src/routers/login-register/login-router.js
+++ src/routers/login-register/login-router.js
@@ -4,25 +4,62 @@ const jwt = require("jsonwebtoken");
 const client = require("../../models/login-register/client");
 const user = require("../../models/login-register/user");
 //LOGIN CLIENTES
-router.post("/client", (req, res) => {
+router.post("/", (req, res) => {
   const { email, password } = req.body;
   client
-    .findOne({ email }, (error, result) => {
+    .findOne({ email }, (error, resultClient) => {
       if (error) res.send({ message: "Error server", status: false });
-      if (!result) res.send({ message: "Email not found", status: false });
-      else {
-        //res.send(email);
-        result.comparePassword(password, (error1, result1) => {
+      
+      //SI no se encuentra en el documento de cliente
+      else if (!resultClient) {
+        user
+          .findOne({ email }, (errorUser, resultUser) => {
+            if (errorUser) res.send({ message: "Error server", status: false });
+            if (!resultUser)
+              res.send({ message: "Email not found", status: false });
+            else {
+              
+              resultUser.comparePassword(password, (errorUser2, resultUser2) => {
+                if (errorUser2)
+                  res.send({ message: "Password incorret", status: false });
+                if (resultUser2) {
+                  //ENVIAR AL CLIENTE
+                  const tokenUser = jwt.sign(
+                    { id: resultUser._id },
+                    "secretuser"
+                  );
+                  res.send({
+                    tokenUser,
+                    status: true,
+                    username: resultUser.username,
+                    email: resultUser.email,
+                    role: resultUser.role,
+                    price: resultUser.price,
+                  });
+                } else {
+                  res.send({
+                    message: "Password/email incorret",
+                    status: false,
+                  });
+                }
+              });
+            }
+          })
+          .populate("price")
+          .populate("role");
+
+      } else {
+        resultClient.comparePassword(password, (error1, result1) => {
           if (error1) res.send({ message: "Password incorret", status: false });
           if (result1) {
             //ENVIAR AL CLIENTE
-            const tokenClient = jwt.sign({ id: result._id }, "secretclient");
+            const tokenClient = jwt.sign({ id: resultClient._id }, "secretclient");
             res.send({
               tokenClient,
               status: true,
-              username: result.username,
-              email: result.email,
-              role: result.role,
+              username: resultClient.username,
+              email: resultClient.email,
+              role: resultClient.role,
             });
           } else {
             res.send({ message: "Password/email incorret", status: false });
@@ -34,39 +71,8 @@ router.post("/client", (req, res) => {
 });
 
 //LOGIN USER
-router.post("/user", (req, res) => {
-  const { email, password } = req.body;
-  user
-    .findOne({ email }, (error, result) => {
-      if (error) res.send({ message: "Error server", status: false });
-      if (!result) res.send({ message: "Email not found", status: false });
-      else {
-        //res.send(email);
-        //console.log(result);
-        result.comparePassword(password, (error1, result1) => {
-          if (error1) res.send({ message: "Password incorret", status: false });
-          if (result1) {
-            //ENVIAR AL CLIENTE
-            const tokenClient = jwt.sign({ id: result._id }, "secretuser");
-            res.send({
-              tokenClient,
-              status: true,
-              username: result.username,
-              email: result.email,
-              role: result.role,
-              price: result.price,
-            });
-          } else {
-            res.send({
-              message: "Password/email incorret",
-              status: false,
-            });
-          }
-        });
-      }
-    })
-    .populate("price")
-    .populate("role");
-});
+// router.post("/user", (req, res) => {
+//   const { email, password } = req.body;
+// });
 
 module.exports = router;
